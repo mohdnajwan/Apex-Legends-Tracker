eb1d8fd8892295f4707d1516d39ecb47d03ee836
mohdnajwan <najwan97@gmail.com>
2020-05-20 03:04:23 +0800

    Added Loading and bug fix

--- routes/profile.js
+++ routes/profile.js
@@ -1,21 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
-router.get('/:platform/:username', async (req,res)=>{
-    const {platform,username} = req.params;
-    try {
-        const headers = {
-            'TRN-Api-Key': process.env.API_KEY
-        }
+router.get("/:platform/:username", async (req, res) => {
+  // Req data from parameter
+  const { platform, username } = req.params;
+  // Try fetching the data
+  try {
+    const headers = {
+      "TRN-Api-Key": process.env.API_KEY,
+    };
 
-        const response = await fetch(`${process.env.URL}/${platform}/${username}`, {headers});
-        const data = await response.json();
-        res.json(data);
-        console.log(data);
-    } catch (error) {
-        console.log(error);
-    }
+    const response = await fetch(`${process.env.URL}/${platform}/${username}`, {
+      headers,
+    });
+    const data = await response.json();
+    res.json(data);
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
