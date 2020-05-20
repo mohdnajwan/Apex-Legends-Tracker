c75d0dd316ad2b1f5d9d65372898c5ce4bec5f64
mohdnajwan <najwan97@gmail.com>
2020-05-21 00:52:11 +0800

    Change static file

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const cors = require('cors');
 require('dotenv/config');
 const Profile = require('./routes/profile');
+
+app.use(cors());
 
 // Route
 app.use('/api', Profile);
 
 // Create Port
 const port = 3001 || process.env.PORT;
+
+// Add Static Path
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // Listen / Create Server
 app.listen(port,()=>{console.log(`Server nodejs running on port: ${port}`)});
