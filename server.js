3240a4e511eb2e641771d5dd9cad44db5e308512
mohdnajwan <najwan97@gmail.com>
2020-05-21 23:33:00 +0800

    New Updates

--- server.js
+++ server.js
@@ -7,19 +7,20 @@ const Profile = require('./routes/profile');
 
 app.use(cors());
 
-// Server Static File
-app.use(express.static(path.join(__dirname + '/build')));
-
 // Route
 app.use('/api', Profile);
 
 // Create Port
 const port = 3001 || process.env.PORT;
 
-// Handle any request that doesnt macth route
-app.get(/.*/,(req,res)=>{
-    res.sendFile(path.join(__dirname + '/build/index.html'));
-});
+if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static(path.join(__dirname, 'build')));
+  
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, 'build','index.html'));
+  });
+}
 
 // Listen / Create Server
 app.listen(port,()=>{console.log(`Server nodejs running on port: ${port}`)});
