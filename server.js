76c4cec8a1ea34b340c1492bb4da787aefc82ef3
mohdnajwan <najwan97@gmail.com>
2020-05-21 23:52:06 +0800

    13 commit

--- server.js
+++ server.js
@@ -15,11 +15,12 @@ const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
     // Set static folder
-    app.use(express.static(path.join(__dirname + '/../build')));
+    app.use(express.static(path.join(__dirname, 'build')));
   
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, '/../build/index.html'));
-  });
+        const index = path.join(__dirname, 'build', 'index.html');
+        res.sendFile(index);
+    });
 }
 
 // Listen / Create Server
